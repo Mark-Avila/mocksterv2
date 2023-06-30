c94210431d1d603db1f80c0e90a1fbf56b81a47a
Mark Avila <avilamark96@gmail.com>
2023-06-30 22:49:30 +0800

    added date fetching for subjects page

--- src/pages/Subjects.tsx
+++ src/pages/Subjects.tsx
@@ -1,7 +1,43 @@
-import { Navbar } from "../components";
+import { useSelector } from "react-redux";
+import { Navbar, PageSpinner, SubjectCard } from "../components";
 import SectionHeader from "../components/SectionHeader";
+import { useState, useEffect } from "react";
+import { RootState } from "../main";
+import { subjectService } from "../services";
+import { SubjectData } from "../types";
 
 function Subjects() {
+  const { data } = useSelector((state: RootState) => state.auth);
+
+  const [subjectData, setSubjectData] = useState<SubjectData[] | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getSubjects = async () => {
+      try {
+        if (!data) {
+          throw Error("Token not found");
+        }
+        const response = await subjectService.getSubjects(data);
+        setSubjectData(response);
+      } catch (err: unknown) {
+        throw Error((err as Error).message);
+      }
+    };
+
+    getSubjects();
+  }, [data]);
+
+  useEffect(() => {
+    if (subjectData) {
+      setIsLoading(false);
+    }
+  }, [subjectData]);
+
+  if (isLoading) {
+    return <PageSpinner />;
+  }
+
   return (
     <>
       <Navbar />
@@ -9,6 +45,18 @@ function Subjects() {
         <SectionHeader>
           Browse <span className="text-red-400">subjects</span>
         </SectionHeader>
+        <ul className="grid grid-cols-4 gap-4 mt-4">
+          {(subjectData as SubjectData[]).map((item: SubjectData) => (
+            <SubjectCard
+              key={item._id}
+              name={item.name}
+              desc={item.desc}
+              onClick={() => {
+                return;
+              }}
+            />
+          ))}
+        </ul>
       </main>
     </>
   );
